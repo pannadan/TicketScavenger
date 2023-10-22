a36e992d75af6d17dfaaae76136102b2e963fdaa
kalratanav <kalratanav@gmail.com>
2023-10-21 22:15:13 -0500

    started formulas

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,7 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import React, { useEffect } from 'react';
 
 function App() {
+
+  useEffect(() => {
+    // Make a GET request to your backend or an external API
+    axios.get('/api/data')
+      .then(response => {
+        console.log(response.data);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }, []); // The empty dependency array ensures this effect runs only once when the component mounts
+
+
   return (
     <div className="App">
       <header className="App-header">
