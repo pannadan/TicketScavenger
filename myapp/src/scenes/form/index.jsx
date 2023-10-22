e489c3ae06cb548ed87571487a23178679a9d608
Ishaan Virani <77121609+Ishaan0601@users.noreply.github.com>
2023-10-21 23:44:12 -0500

    Fixed form fields

--- myapp/src/scenes/form/index.jsx
+++ myapp/src/scenes/form/index.jsx
@@ -1,4 +1,4 @@
-import { Box, Button, TextField } from '@mui/material';
+import { Box, Button, TextField, MenuItem, Select, FormControl, InputLabel } from '@mui/material';
 import { Formik } from "formik";
 import * as yup from "yup";
 import useMediaQuery from "@mui/material/useMediaQuery";
@@ -19,15 +19,31 @@ const initialValues = {
 };
 
 const userSchema = yup.object().shape({
-    age: yup.string().required("required"),
-    retireAge:yup.string().required("required"),
-    livingCost:yup.string().required("required"),
-    savings:yup.string().required("required"),
-    investments:yup.string().required("required"),
-    apy:yup.string().required("required"),
-    yearsOfRet:yup.string().required("required"),
-    monthlyCont:yup.string().required("required"),
-    companyMatching:yup.string().required("required"),
+    age: yup.number()
+    .required("required")
+    .positive("Must be positive"),
+    retireAge:yup.number()
+    .required("required")
+    .positive("Must be positive"),
+    livingCost:yup.number()
+    .required("required")
+    .positive("Must be positive"),
+    savings:yup.number()
+    .required("required")
+    .positive("Must be positive"),
+    investments:yup.number()
+    .required("required"),
+    apy:yup.number()
+    .required("required"),
+    yearsOfRet:yup.number()
+    .required("required")
+    .positive("Must be positive"),
+    monthlyCont:yup.number()
+    .required("required")
+    .positive("Must be positive"),
+    companyMatching:yup.number()
+    .required("required")
+    .positive("Must be positive"),
     fundType:yup.string().required("required"),
 });
 
@@ -69,7 +85,7 @@ const Form = () => {
                                 onBlur = {handleBlur}
                                 onChange = {handleChange}
                                 value = {values.age}
-                                name = "Age"
+                                name = "age"
                                 error = {!!touched.date && !!errors.date}
                                 helperText = {touched.date && errors.date}
                                 sx = {{ gridColumn: "span 2"}}
@@ -165,19 +181,46 @@ const Form = () => {
                                 helperText = {touched.team && errors.team}
                                 sx = {{ gridColumn: "span 2"}}
                             />
-                             <TextField
+                            <FormControl
                                 fullWidth
-                                variant = "filled"
-                                type = "dropdown"
-                                label = "Account Type"
-                                onBlur = {handleBlur}
-                                onChange = {handleChange}
-                                value = {values.team}
-                                name = "fundType"
-                                error = {!!touched.team && !!errors.team}
-                                helperText = {touched.team && errors.team}
-                                sx = {{ gridColumn: "span 2"}}
-                            />
+                                variant="filled"
+                                sx={{ gridColumn: "span 2" }}
+                            >
+                                <InputLabel>Account Type</InputLabel>
+                                <Select
+                                    label="Account Type"
+                                    onBlur={handleBlur}
+                                    onChange={handleChange}
+                                    value={values.fundType}
+                                    name="fundType"
+                                    error={touched.fundType && !!errors.fundType}
+                                >
+                                    <MenuItem value="roth">Roth IRA</MenuItem>
+                                    <MenuItem value="traditonal">Traditional IRA</MenuItem>
+                                    <MenuItem value="four">401K</MenuItem>
+                                </Select>
+                            </FormControl>
+                             {/* <TextField
+                                multiline
+                                fullWidth
+                                variant="filled"
+                                label="Account Type"
+                                onBlur={handleBlur}
+                                onChange={handleChange}
+                                name="fundType"
+                                error={!!touched.team && !!errors.team}
+                                helperText={touched.team && errors.team}
+                                sx={{ gridColumn: "span 2" }}
+                                >
+                                <Select
+                                    value={values.fundType}
+                                    onChange={handleChange}
+                                >
+                                    <MenuItem value="option1">Option 1</MenuItem>
+                                    <MenuItem value="option2">Option 2</MenuItem>
+                                    <MenuItem value="option3">Option 3</MenuItem>
+                                </Select>
+                                </TextField> */}
                         </Box>
                         <Box display = "flex" justifyContent = "end" mt = "20px">
                             <Button type = "submit" color = "secondary" variant = "contained">
